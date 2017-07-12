9d981cfb0182d6d737866ee1b3efab21cfde6cb6
Martin Madsen <mmadsen@allegient.com>
2017-07-12 09:36:05 -0400

    Updated repo

---
+++ src/AW_customerQuery.sql
@@ -0,0 +1,15 @@
+﻿select 
+	fc.ProductKey,
+	fc.CustomerKey,
+	fc.OrderDateKey,
+	fc.ShipDateKey,
+	fc.SalesOrderNumber,
+	REPLACE(dc.AddressLine1, ',' ,' ' ) as AddressLine1,
+	dc.AddressLine2,
+	dg.City,
+	dg.StateProvinceName,
+	dg.EnglishCountryRegionName
+ from FactInternetSales as fc
+JOIN DimCustomer as dc on fc.CustomerKey=dc.CustomerKey
+and fc.ProductKey IN ( 220, 221, 222 )
+JOIN DimGeography as dg on dc.GeographyKey=dg.GeographyKey
